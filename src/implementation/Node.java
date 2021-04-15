7d093654c3fa7df126c286714c321314a7d28b3e
Pradhum Gupta <pradhumgp@gmail.com>
2021-04-15 21:57:26 +0530

    add fields and constructor

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -1,5 +1,13 @@
 package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
-    //complete node class
+    private int data;
+    private Node<E> left;
+    private Node<E> right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
